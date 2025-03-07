6bff6e202857939f230abcce7aa9c08fed6ea937
Yashi Chauhan <64953869+yashi-coder@users.noreply.github.com>
2025-03-07 14:56:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,32 @@
 ##this will work as variable set is applied.
+variable "client_id" {
+  description = "Azure Service Principal client ID"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Azure Service Principal client secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
+
+
 provider "azurerm" {
   features {}
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
-  subscription_id="b8e2596b-e946-44ac-a26d-b40cbda624ef"
+  subscription_id = var.subscription_id
    resource_provider_registrations = "none"
 }
 
